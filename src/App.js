0535ddfb58bb40278db8e20d4eef7c897667c225
dqhuang <huangdarren1106@gmail.com>
2022-05-31 18:45:42 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,122 +1,63 @@
-import React, { Component } from "react";
-import * as $ from "jquery";
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { authEndpoint, clientId, redirectUri, scopes } from "./config_example";
 import hash from "./hash";
 import Player from "./Player";
+import TenTracks from "./TenTracks"
+import GraphScreen from "./GraphScreen";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0
-      },
-      is_playing: "Paused",
-      progress_ms: 0,
-      no_data: false,
-    };
+export default function App() {
+  const [token, setToken] = useState(null);
+  const [topTracks, setTopTracks] = useState([]);
+  const [topTracksActivated, setTopTracksActivated] = useState(false);
+  const [topArtists, setTopArtists] = useState([]);
+  const [topArtistsActivated, setTopArtistsActivated] = useState(false);
 
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-    this.tick = this.tick.bind(this);
+  const getTopTracks = (token) => {
+    /*
+    const headers = {
+      "Authorization": "Bearer " + token
+  };
+    axios.get("https://api.spotify.com/v1/me/top/tracks", {headers}).then((response) => {
+      console.log(response.data);
+      setTopTracks(response.data.items);
+      setTopTracksActivated(true);
+    })
+    */
+    setTopTracksActivated(true)
   }
 
 
 
-  componentDidMount() {
-    // Set token
-    let _token = hash.access_token;
-
-    if (_token) {
-      // Set token
-      this.setState({
-        token: _token
-      });
-      this.getCurrentlyPlaying(_token);
+  useEffect(() => {
+    var mToken = hash.access_token;
+    if (mToken) {
+      setToken(mToken);
     }
 
-    // set interval for polling every 5 seconds
-    this.interval = setInterval(() => this.tick(), 5000);
-  }
+  })
+  return (
+    <div>
+      
+      <body className="App-body">
+        {!token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+              "%20"
+            )}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {token && (
+          <GraphScreen token={token} />
+        )}
+      </body>
+    </div>
+  );
 
-  componentWillUnmount() {
-    // clear the interval to save resources
-    clearInterval(this.interval);
-  }
-
-  tick() {
-    if(this.state.token) {
-      this.getCurrentlyPlaying(this.state.token);
-    }
-  }
-
-
-  getCurrentlyPlaying(token) {
-    // Make a call using the token
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: data => {
-        // Checks if the data is not empty
-        if(!data) {
-          this.setState({
-            no_data: true,
-          });
-          return;
-        }
-
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms,
-          no_data: false /* We need to "reset" the boolean, in case the
-                            user does not give F5 and has opened his Spotify. */
-        });
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
-          {this.state.token && !this.state.no_data && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-              progress_ms={this.state.progress_ms}
-            />
-          )}
-          {this.state.no_data && (
-            <p>
-              You need to be playing a song on Spotify, for something to appear here.
-            </p>
-          )}
-        </header>
-      </div>
-    );
-  }
 }
 
-export default App;
--- src/config_example.js
+++ src/config_example.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "YOUR_CLIENT_ID_HERE";
+export const clientId = "e06a8e4468034056a35e699e61e7b0cb";
 export const redirectUri = "http://localhost:3000/redirect";
 export const scopes = [
     "user-top-read",
