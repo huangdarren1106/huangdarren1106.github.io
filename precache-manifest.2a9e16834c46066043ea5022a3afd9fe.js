affd673cc56e02b969af134ce95645ae060936d9
dqhuang <huangdarren1106@gmail.com>
2022-05-31 20:00:30 -0700

    Updates

---
+++ precache-manifest.2a9e16834c46066043ea5022a3afd9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04862623a58cb34d2645",
+    "url": "/static/css/main.db583d5c.chunk.css"
+  },
+  {
+    "revision": "04862623a58cb34d2645",
+    "url": "/static/js/main.7914db34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7116248c7c0149acbef",
+    "url": "/static/js/2.65e68182.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4414174cbea512776e066daf7a505e19",
+    "url": "/index.html"
+  }
+];
